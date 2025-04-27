13242143d4b86fab5664c8def36ee380434c7e31
Tomanović Milica <114880171+milica-gz@users.noreply.github.com>
2025-04-27 12:03:55 +0000

    Add validation for student signup and activity capacity

--- src/app.py
+++ src/app.py
@@ -55,13 +55,19 @@ def get_activities():
 @app.post("/activities/{activity_name}/signup")
 def signup_for_activity(activity_name: str, email: str):
     """Sign up a student for an activity"""
+    # Validate student is not already signed up
+    for activity in activities.values():
+        if email in activity["participants"]:
+            raise HTTPException(status_code=400, detail="Student already signed up")
     # Validate activity exists
     if activity_name not in activities:
         raise HTTPException(status_code=404, detail="Activity not found")
 
     # Get the specificy activity
     activity = activities[activity_name]
-
+    # Check if the activity is full
+    if len(activity["participants"]) >= activity["max_participants"]:
+        raise HTTPException(status_code=400, detail="Activity is full")
     # Add student
     activity["participants"].append(email)
     return {"message": f"Signed up {email} for {activity_name}"}
